c30269a908506b409dd445c20be52668a97f7d1a
Elnar Ibragimov <4393143+Djelnar@users.noreply.github.com>
2021-10-28 14:50:28 +0300

    Fix

--- server.js
+++ server.js
@@ -1,70 +1,52 @@
-const
-    server = new (require('koa')),
-    logger = require('koa-morgan'),
-    _ = require('lodash'),
-    serve = require('koa-static'),
-    path = require('path'),
-    views = require('koa-views'),
-    router = require('koa-router')(),
-    bodyParser = require('koa-body'),
-    WebSocketServer = new require('ws')
-
-let wss = new WebSocketServer.Server({port: 1488})
+//@ts-check
+const express = require("express");
+const ws = require("ws");
+const path = require("path");
+const http = require("http");
+const _ = require("lodash");
 
 let store = {
-    balls: [
+  balls: {},
+  clients: [],
+};
 
-    ],
-    clients: [
+const app = express();
+app.use(express.static(path.resolve(process.cwd(), "static")));
 
-    ]
-}
+const server = http.createServer(app);
 
-wss.on('connection', (e) => {
-    let uniqId = store.clients.length
-    store.clients.push(e)
-    store.balls.push({
-        x: 0,
-        y: 0,
-        uniqId
-    })
-    
-    e.on('message', (m) => {
-        let msg = JSON.parse(m)        
-        store.balls[uniqId] = {
-            x: msg.x,
-            y: msg.y,
-            uniqId
-        }
-        store.clients.forEach((el, idx, arr) => {
-            try {
-                el.send(JSON.stringify(_.compact(store.balls)))
-            } catch(err) {
-                console.log('Websocket error: %s', err);
-            }
-        })
-    })
+let wss = new ws.Server({ server: server });
 
-    e.on('close', (params) => {
-        store.balls[uniqId] = null
-    })
-})
+wss.on("connection", (e) => {
+  let uniqId = _.uniqueId();
+  store.clients.push(e);
+  store.balls[uniqId] = {
+    x: 0,
+    y: 0,
+    uniqId,
+  };
 
+  e.on("message", (m) => {
+    let msg = JSON.parse(m);
+    store.balls[uniqId] = {
+      x: msg.x,
+      y: msg.y,
+      uniqId,
+    };
+    store.clients.forEach((el, idx, arr) => {
+      try {
+        el.send(JSON.stringify(_.compact(Object.values(store.balls))));
+      } catch (err) {
+        console.log("Websocket error: %s", err);
+      }
+    });
+  });
 
+  e.on("close", (params) => {
+    store.balls[uniqId] = null;
+  });
+});
 
-
-
-
-
-router.get('/', async ctx => {
-    await ctx.render('index')
-})
-
-server
-    .use(views(path.join(__dirname, '/views'), { extension: 'ejs' }))
-    .use(serve('static'))
-    // .use(logger('tiny'))
-    .use(router.routes())
-    .listen(1338, () => {
-    console.log(`Listening...`)
-})
+server.listen(1338, () => {
+  console.log(`Listening...`);
+});
--- static/main.js
+++ static/main.js
@@ -1,35 +1,41 @@
-const socket = new WebSocket(`ws://${window.location.hostname}:1488`)
+const socket = new WebSocket(
+  `ws://${window.location.hostname}:${window.location.port}`
+);
 
 function createPoint(props) {
-    let messageElem = document.createElement('div')
-    messageElem.appendChild(document.createTextNode(props.uniqId))
-    messageElem.className = 'thing'
-    messageElem.id = props.uniqId
-    messageElem.style.top = `${props.y}px`
-    messageElem.style.left = `${props.x}px`
-    document.getElementById('root').appendChild(messageElem)
+  let messageElem = document.createElement("div");
+  messageElem.appendChild(document.createTextNode(props.uniqId));
+  messageElem.className = "thing";
+  messageElem.id = props.uniqId;
+
+  messageElem.style.setProperty("--px", props.x + "px");
+  messageElem.style.setProperty("--py", props.y + "px");
+
+  document.getElementById("root").appendChild(messageElem);
 }
 
+function updatePoint(props) {
+  let messageElem = document.getElementById(props.uniqId);
 
-
-socket.onopen = function() {
-    
+  messageElem.style.setProperty("--px", props.x + "px");
+  messageElem.style.setProperty("--py", props.y + "px");
 }
 
-socket.onmessage = function(event) {
-    let inc = JSON.parse(event.data)
-    inc.forEach((el, idx, arr) => {
-        if (document.getElementById(el.uniqId)) {
-            document.getElementById(el.uniqId).remove()
-        }
-        createPoint(el)
-    })
-}
+socket.onmessage = function (event) {
+  let inc = JSON.parse(event.data);
+  inc.forEach((el, idx, arr) => {
+    if (document.getElementById(el.uniqId)) {
+      updatePoint(el);
+    } else {
+      createPoint(el);
+    }
+  });
+};
 
-document.querySelector('#root').addEventListener('mousemove', (e) => {
-    let msg = JSON.stringify({
-        x: e.clientX,
-        y: e.clientY
-    })
-    socket.send(msg)
-})
+document.querySelector("#root").addEventListener("mousemove", (e) => {
+  let msg = JSON.stringify({
+    x: e.clientX,
+    y: e.clientY,
+  });
+  socket.send(msg);
+});
